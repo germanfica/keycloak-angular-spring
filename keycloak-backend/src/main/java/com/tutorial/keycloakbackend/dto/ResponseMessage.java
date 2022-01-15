bfd3737f0000f74c7ce0d2ccb01d0eb3deb9f339
German Fica <germafica@gmail.com>
2022-01-15 05:05:38 -0300

    Delete boilerplate code

--- keycloak-backend/src/main/java/com/tutorial/keycloakbackend/dto/ResponseMessage.java
+++ keycloak-backend/src/main/java/com/tutorial/keycloakbackend/dto/ResponseMessage.java
@@ -1,21 +1,11 @@
 package com.tutorial.keycloakbackend.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseMessage {
-
     private String message;
-
-    public ResponseMessage() {
-    }
-
-    public ResponseMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
