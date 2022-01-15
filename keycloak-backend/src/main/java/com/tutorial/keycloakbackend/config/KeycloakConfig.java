1fa26f7b0ad0cac556b049779645a922222acbe9
German Fica <germafica@gmail.com>
2022-01-15 04:36:45 -0300

    Fix circular references
    The message error:
    
    ***************************
    APPLICATION FAILED TO START
    ***************************
    
    Description:
    
    The dependencies of some of the beans in the application context form a
    cycle:
    
    ┌──->──┐
    |  keycloakWebSecurityConfig (field private
    org.keycloak.adapters.KeycloakConfigResolver
    org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter.keycloakConfigResolver)
    └──<-──┘
    
    Action:
    
    Relying upon circular references is discouraged and they are prohibited
    by default. Update your application to remove the dependency cycle
    between beans. As a last resort, it may be possible to break the cycle
    automatically by setting spring.main.allow-circular-references to true.

--- keycloak-backend/src/main/java/com/tutorial/keycloakbackend/config/KeycloakConfig.java
+++ keycloak-backend/src/main/java/com/tutorial/keycloakbackend/config/KeycloakConfig.java
@@ -1,49 +1,29 @@
 package com.tutorial.keycloakbackend.config;
 
-
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 
-@KeycloakConfiguration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
-public class KeycloakConfig extends KeycloakWebSecurityConfigurerAdapter {
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        KeycloakAuthenticationProvider provider = new KeycloakAuthenticationProvider();
-        provider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-        auth.authenticationProvider(provider);
-    }
-
+@Configuration
+public class KeycloakConfig {
+    // == beans methods ==
     @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new NullAuthenticatedSessionStrategy();
+    protected SessionRegistry buildSessionRegistry() {
+        return new SessionRegistryImpl();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
-        super.configure(http);
-        http
-                .authorizeRequests()
-                .anyRequest().permitAll();
-        http.csrf().disable();
-    }
-
+    /**
+     * By Default, the Spring Security Adapter looks for a keycloak.json configuration
+     * file. You can make sure it looks at the configuration provided by the Spring
+     * Boot Adapter by adding this bean.
+     *
+     * Docs: https://www.keycloak.org/docs/latest/securing_apps/index.html#using-spring-boot-configuration
+     *
+     * @return
+     */
     @Bean
     public KeycloakConfigResolver keycloakConfigResolver(){
         return new KeycloakSpringBootConfigResolver();
